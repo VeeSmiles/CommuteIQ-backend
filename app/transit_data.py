b06d455e32d79de5cf63864dd852beb830f1d503
Vivian Ndung'u <ndunguvihvy@gmail.com>
2026-08-20 09:41:00 +0300

    Added transit_data.py

---
+++ app/transit_data.py
@@ -0,0 +1,8 @@
+# app/transit_data.py
+MATATU_ROUTES = {}
+
+def get_walk_to_transit(*args, **kwargs):
+    return {"note": "transit_data not yet implemented"}
+
+def get_matatu_route(*args, **kwargs):
+    return {"note": "transit_data not yet implemented"}
